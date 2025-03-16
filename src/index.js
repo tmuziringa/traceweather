7ac404a0ac80d0e9603ab856e739dd6027958e99
tmuziringa <tmuziringa@gmail.com>
2025-03-16 01:44:19 +0000

    injected weather data

--- src/index.js
+++ src/index.js
@@ -2,11 +2,36 @@ function refreshWeather(response){
     let temperatureElement=document.querySelector("#temperature");
     let temperature= response.data.temperature.current;
     let cityElement=document.querySelector("#city");
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windSpeedElement=document.querySelector("#wind-speed");
+    let timeElement=document.querySelector("#time");
+    let date=new Date(response.data.time *1000);
+
     cityElement.innerHTML=response.data.city;
     temperatureElement.innerHTML=Math.round(temperature);
+    descriptionElement.innerHTML=response.data.condition.description;
+    humidityElement.innerHTML= `${response.data.temperature.humidity}%`;
+    windSpeedElement.innerHTML=`${response.data.wind.speed}km/h`;
+    timeElement.innerHTML= formatDate(date);
+
 
 }
+function formatDate(date){
+    let minutes= date.getMinutes();
+    let hours=date.getHours();
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",];
+    let day= days[date.getDay()];
+if (minutes<10) {
+minutes=`0${minutes}`;
+}
+if(hours<10){
+    hours=`0${hours}`;
+}
+    return `${day} ${hours}:${minutes}`
 
+
+}
 function searchCity(city){
 let apiKey="0bb03dodf4t67bd073b315986695ca76";
 let apiUrl=`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
